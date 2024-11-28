8f8384f01e31d453b3381f3d35308cbe9dccc7c4
antonio <antoniohurtadoredondo@gmail.com>
2024-11-28 13:09:48 +0100

    Primera configuración

---
+++ apache2-provision.sh
@@ -0,0 +1,26 @@
+#
+# Copiar ficheros de configuración de apache2 que incluyen:
+#
+# - apache2.conf,
+# - apolo.olimpo.test.conf
+# - atenea.olimpo.test.conf
+#
+
+# tu código aquí
+cp -v /vagrant/files/apache2/apache2.conf /etc/apache2/
+cp -v /vagrant/files/apolo.olimpo.test/apolo.olimpo.test.conf /etc/apache2/sites-available/
+cp -v /vagrant/files/atenea.olimpo.test/atenea.olimpo.test.conf /etc/apache2/sites-available/
+#
+# Habilitar los servidores virtuales
+#
+
+# tu código aquí
+a2dissite 000-default.conf
+a2ensite apolo.olimpo.test.conf
+a2ensite atenea.olimpo.test.conf
+#
+# Reiniciar el servicio
+#
+
+# tu código aquí
+systemctl restart apache2
